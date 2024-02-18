bb860a7be307ebc245552e7d32415724d7d866a9
pwangy <blipsandclicks@gmail.com>
2024-02-17 20:45:52 -0800

    lab complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,31 @@
-function NavBar() {
+const NavBar = () => {
 	return (
 		<nav>
 			<a href='#home'>I'm a link!</a>
 		</nav>
-	)
-}
+)}
 
-function Home() {
+const Home = () => {
 	return (
 		<div id='home'>
 			<h1>Home</h1>
 		</div>
-	)
-}
+)}
 
-{
-	/* write an <About> component here */
+const About = () => {
+  return (
+  <div id="about">
+    <h2>2Here's a little something about moi.</h2>
+  </div>)
 }
 
 function App() {
 	return (
 		<div>
 			<NavBar />
-			{/* add the <Home> component here */}
-			{/* add your <About> component here */}
+			<Home />
+			<About />
 		</div>
-	)
-}
+)}
 
 export default App

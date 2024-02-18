26b698e8787b0855a6822e7297486474764665c0
pwangy <blipsandclicks@gmail.com>
2024-02-17 20:37:03 -0800

    clean code

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,31 @@
-import React from "react";
-
 function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
+	return (
+		<nav>
+			<a href='#home'>I'm a link!</a>
+		</nav>
+	)
 }
 
 function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
+	return (
+		<div id='home'>
+			<h1>Home</h1>
+		</div>
+	)
 }
 
-{/* write an <About> component here */}
-
+{
+	/* write an <About> component here */
+}
 
 function App() {
-  return (
-    <div>
-      <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
-    </div>
-  );
+	return (
+		<div>
+			<NavBar />
+			{/* add the <Home> component here */}
+			{/* add your <About> component here */}
+		</div>
+	)
 }
 
-export default App;
+export default App
